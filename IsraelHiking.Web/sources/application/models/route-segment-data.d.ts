aa24b9197f2c0e9a40c1bb9b830c1d86ce21dcdc
Harel Mazor <harel.mazor@gmail.com>
2020-05-09 21:49:52 +0300

    NorthArrow - Replaced bike routing with car routing

--- IsraelHiking.Web/sources/application/models/route-segment-data.d.ts
+++ IsraelHiking.Web/sources/application/models/route-segment-data.d.ts
@@ -1,6 +1,6 @@
 ﻿import { LatLngAlt, ILatLngTime } from "./models";
 
-export type RoutingType = "Hike" | "Bike" | "4WD" | "None";
+export type RoutingType = "Hike" | "Bike" | "Car" | "4WD" | "None";
 
 export interface RouteSegmentData {
     routePoint: LatLngAlt;
