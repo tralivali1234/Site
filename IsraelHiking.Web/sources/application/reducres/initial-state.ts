13b3d8cca689ffcc1b46b87a361aab2a9e650b69
Harel Mazor <harel.mazor@gmail.com>
2020-09-08 09:43:06 +0300

    NorthArrow - Change sattelite max zoom

--- IsraelHiking.Web/sources/application/reducres/initial-state.ts
+++ IsraelHiking.Web/sources/application/reducres/initial-state.ts
@@ -64,7 +64,7 @@ export const initialState =
                     isOfflineAvailable: false,
                     isOfflineOn: false,
                     minZoom: 7,
-                    maxZoom: 16
+                    maxZoom: 20
                 }
             ],
             overlays: [
